c03cb67394567e832f34653af2d662ef254c62eb
sidneyabush <sidneybush@Sidneys-MacBook-Air.local>
2017-03-06 21:28:07 -0700

    Repeated LL cal process now averages lines, cutoffs and min values.

--- PrecipRunoff/LL Calibration/ExtractRepeatedCalValues.m
+++ PrecipRunoff/LL Calibration/ExtractRepeatedCalValues.m
@@ -37,6 +37,9 @@ end
 
 % Check to make sure all pairs of truth-measured files are present.
 %% For each site, find best fit of each repeated calibrations
+% Create structure to store linear equations for each level logger. 
+equations= struct;
+
 for i = 1:length(allRawTruthDirNames)
     folderName = allRawTruthDirNames{i};
     % Check that there are the proper amount of csv files in the folder.
@@ -52,14 +55,11 @@ for i = 1:length(allRawTruthDirNames)
         return;
     end
     
-    % We'll store the coefficients for each repeated run in these.
-    allLogCoefs = {};
-    allLinearCoefs = {};
-    
-    % TODO: figure out a way to pass out this function from the FindBestFit
-    % function
-    logFunc = @(B,x) B(1).*log(x + B(2)) + B(3);
-    
+    % We'll store values here for averaging after processing all reps. 
+    allLinearCoefs1 = [];
+    allLinearCoefs2 = [];
+    cutoffInMM = [];
+    minRawWaterLevel = [];
     % Prepare a figure to contain all lines of best fit from each
     % repetition. 
     figure;
@@ -72,7 +72,7 @@ for i = 1:length(allRawTruthDirNames)
         % Perform the calibration.
         cutoffBeginning = 6; % 1 = no cutuff.
         cutoffInLiters = 4;
-        slopeSplit = cutoffInLiters * 2; % Two data points per liter.
+        linearSplit = cutoffInLiters * 2; % Two data points per liter.
         measuredFilename = [rawMeasuredDataDir folderName '/' repeatedMeasuredFiles(j).name];
         truthFilename = [rawTruthDataDir folderName '/' repeatedMeasuredFiles(j).name];
         [lineFit1, lineFit2, selectedMeasurementMM, truthVol] = FindBestFit(truthFilename,...
@@ -82,18 +82,38 @@ for i = 1:length(allRawTruthDirNames)
         plot(selectedMeasurementMM, truthVol, 'o', 'Color', cmap(j,:));
         
          % Plot all the different lines of best fit on one graph for comparison
-        linearSamples1 = selectedMeasurementMM(cutoffBeginning):0.1:selectedMeasurementMM(slopeSplit);
-        linearSamples2 = selectedMeasurementMM(slopeSplit):10:selectedMeasurementMM(end);
+        linearSamples1 = selectedMeasurementMM(cutoffBeginning):0.1:selectedMeasurementMM(linearSplit);
+        linearSamples2 = selectedMeasurementMM(linearSplit):10:selectedMeasurementMM(end);
         calculatedLinearVolumes1 = polyval(lineFit1, linearSamples1);
         calculatedLinearVolumes2 = polyval(lineFit2, linearSamples2);
         plot(linearSamples1, calculatedLinearVolumes1, 'Color', cmap(j,:));
         plot(linearSamples2, calculatedLinearVolumes2, 'Color', cmap(j,:));
         
         % Store the results of this trial
-        allLinearCoefs1{j} = lineFit1;
-        allLinearCoefs2{j} = lineFit2;
+        allLinearCoefs1 = [allLinearCoefs1; lineFit1];
+        allLinearCoefs2 = [allLinearCoefs2; lineFit2];
+        cutoffInMM = [cutoffInMM; selectedMeasurementMM(linearSplit)];
+        minRawWaterLevel = [minRawWaterLevel; selectedMeasurementMM(cutoffBeginning)];
         disp(['Linear1 Coefs for: ' repeatedMeasuredFiles(j).name 'is: ' num2str(lineFit1)]);
         disp(['Linear2 Coefs for: ' repeatedMeasuredFiles(j).name 'is: ' num2str(lineFit2)]);
     end
     hold off;  
+    
+    % Remove 'Repeated' from the filename so it matches the name of the
+    % single-run calibrations. 
+    name = strrep(folderName, 'Repeated', '');
+    % Average and store the lines of best fit.
+    %
+    %
+    % DANGER - is it mathematically sound to simply average the slopes and
+    % intercepts as we do here?
+    %
+    %
+    equations.(name).linearFit1 =       mean(allLinearCoefs1);
+    equations.(name).linearFit2 =       mean(allLinearCoefs2);
+    equations.(name).cutoffInMM =       mean(cutoffInMM);
+    equations.(name).minRawWaterLevel = mean(minRawWaterLevel);
 end
+% Save our structure to a .mat file so we can load it elsewhere
+saveDir = [cleanedDataDir 'LLRepEquations'];
+save(saveDir, 'equations');
