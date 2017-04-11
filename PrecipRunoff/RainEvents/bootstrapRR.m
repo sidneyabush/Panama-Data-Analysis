c6cffcde49f910592725c073470b7f46331c404b
sidneyabush <sidneybush@Sidneys-MacBook-Air.local>
2017-04-10 23:52:54 -0600

    Include the merged, bootstrapped data in the plotGoodRR box plots.

--- PrecipRunoff/RainEvents/bootstrapRR.m
+++ PrecipRunoff/RainEvents/bootstrapRR.m
@@ -2,7 +2,7 @@
 
 %% Determine which events to bootstrap.
 matFolder = 'RainEventFigures/All_Runoff/';
-eventFolder = 'Merge';
+eventFolder = 'Merge/';
 
 % Load the mat file containing our event data.
 load([matFolder 'allEvents.mat']);
@@ -10,16 +10,16 @@ load([matFolder 'allEvents.mat']);
 % Get all the figures in the desired folder.
 % matFigs = dir([matFolder eventFolder '/MAT*.fig']);
 % pasFigs = dir([matFolder eventFolder '/PAS*.fig']);
-eventFiles = dir([matFolder eventFolder '/*.fig']);
+eventFiles = dir([matFolder eventFolder '*.fig']);
 % allFigs = {matFigs, pasFigs};
 
 % allEvents = {MAT_Events, PAS_Events};
 % siteNames = {'MAT', 'PAS'};
 
 % Create a structure to save the bootstrapped RR data.
-data = struct();
-data.MAT = [];
-data.PAS = [];
+mergeRR = struct();
+mergeRR.MAT = struct();
+mergeRR.PAS = struct();
 
 % Extract, using parentheses in the regex, the event number and TB or LL.
 pattern = '([A-T]{3})_event_(\d+)_([L-T][B-L]).fig';
@@ -32,7 +32,7 @@ for fileNum = 1:length(eventFiles)
     evtIdx = str2double(tokens{1}{1}{2});
     type = tokens{1}{1}{3};
 
-    % Obtain all 6 types of runoff data.
+    % Are we looking at an event from the MAT or the PAS?
     switch site
     case 'MAT'
         evtArray = MAT_Events;
@@ -40,15 +40,16 @@ for fileNum = 1:length(eventFiles)
         evtArray = PAS_Events;
     end
 
-    % allRunoff = [evtArray(evtIdx).allRunoff];
-    % Store all the runoff totals in one vector so we can sample it.
+    % Store all 6 runoff totals in one vector so we can sample from it.
     runoffTotals = [];
     numRunoffSources = 6; % Don't include the celestino data for MAT.
     for runNum = 1:numRunoffSources
         runoffTotals = [runoffTotals; evtArray(evtIdx).allRunoff(runNum).getTotal('mod')];
     end
+
+    % Precip is also needed for the RR calculation.
     totalPrecip = evtArray(evtIdx).getTotal();
-    % display(runoffTotals);
+
     % Perform the bootstrapping.
     % calcRR = @(runoff) (sum(runoff) / (totalPrecip * length(runoff));
     calcRR = @(runoff) (mean(runoff) / totalPrecip);
@@ -57,11 +58,13 @@ for fileNum = 1:length(eventFiles)
     % histogram(bootstat);
 
     % Store the data in a struct for easy processing.
-    evtInfo = struct('num', evtIdx, 'RRs', bootstat);
-    data.(site) = [data.(site) evtInfo]; 
+    fieldName = ['evt' num2str(evtIdx)];
+    mergeRR.(site).(fieldName) = bootstat;
+    % evtInfo = struct('num', evtIdx, 'RRs', bootstat);
+    % data.(site) = [data.(site) evtInfo];
 
 end
 
-
-
-%% Bootstrap events.
+% Save bootstrapping data for use elsewhere.
+saveDir = [matFolder eventFolder 'mergeRR'];
+save(saveDir, 'mergeRR');
--- PrecipRunoff/RainEvents/plotGoodRR.m
+++ PrecipRunoff/RainEvents/plotGoodRR.m
@@ -4,105 +4,123 @@
 
 matFolder = 'RainEventFigures/All_Runoff/';
 eventFolder = 'Good';
+mergeFolder = 'Merge';
 
-% Load the .mat file containing our event data
+% Load the .mat file containing our event data.
 load([matFolder 'allEvents.mat']);
+% Load the .mat file containing our merged RR data.
+load([matFolder mergeFolder '/mergeRR.mat']);
 
 % Get all the figures in the desired folder.
 matFigs = dir([matFolder eventFolder '/MAT*.fig']);
 pasFigs = dir([matFolder eventFolder '/PAS*.fig']);
 allFigs = {matFigs, pasFigs};
 allEvents = {MAT_Events, PAS_Events};
-siteNames = {'MAT', 'PAS'};
+sites = {'MAT', 'PAS'};
 
 % Create a structure to store the RR data, Intensity data, etc.
 data = struct();
-data.(siteNames{1}) = struct();
-data.(siteNames{2}) = struct();
+data.(sites{1}) = struct();
+data.(sites{2}) = struct();
 
 % Extract, using parentheses in the regex, the event number and TB or LL.
 pattern = 'event_(\d+)_([L-T][B-L]).fig';
-for j = 1:length(allFigs)
+% For both the MAT and PAS field sites.
+for j = 1:length(sites)
     tokens = regexp({allFigs{j}.name}, pattern, 'tokens');
 
     % Store the event number and type (TB or LL).
-    data.(siteNames{j}).RR = [];
-    data.(siteNames{j}).celestinoRR = [];
-    data.(siteNames{j}).startTimes = [];
-    data.(siteNames{j}).endTimes = [];
-    data.(siteNames{j}).PI = [];
-    data.(siteNames{j}).AvgI = [];
-    data.(siteNames{j}).celestinoAvgI = [];
-    data.(siteNames{j}).celestinoPI = [];
+    data.(sites{j}).RR = [];
+    data.(sites{j}).celestinoRR = [];
+    data.(sites{j}).startTimes = [];
+    data.(sites{j}).endTimes = [];
+    data.(sites{j}).PI = [];
+    data.(sites{j}).AvgI = [];
+    data.(sites{j}).celestinoAvgI = [];
+    data.(sites{j}).celestinoPI = [];
     cells = [tokens{:}];
+    % For every event in the "good" folder, for the current site.
     for i = 1:length(cells)
         evtIdx = str2double(cells{i}{1});
-        data.(siteNames{j}).RR = [data.(siteNames{j}).RR allEvents{j}(evtIdx).stats.mod.RR.(cells{i}{2}).precip];
-        % Notify if we just found something with a RR > 1.
-        if data.(siteNames{j}).RR(end) >= 1
-          warning(['RR >= 1 for: ' siteNames{j} ' ' num2str(evtIdx)]);
-        end
-        data.(siteNames{j}).startTimes = [data.(siteNames{j}).startTimes allEvents{j}(evtIdx).startTime];
-        data.(siteNames{j}).endTimes = [data.(siteNames{j}).endTimes allEvents{j}(evtIdx).endTime];
-        data.(siteNames{j}).PI = [data.(siteNames{j}).PI allEvents{j}(evtIdx).stats.mod.int.peak.precip];
-        data.(siteNames{j}).AvgI = [data.(siteNames{j}).AvgI allEvents{j}(evtIdx).stats.mod.int.avg.precip];
 
-        if strcmpi(siteNames{j}, 'MAT')
-            data.(siteNames{j}).celestinoRR = [data.(siteNames{j}).celestinoRR allEvents{j}(evtIdx).stats.mod.RR.(cells{i}{2}).addl];
-            if data.(siteNames{j}).celestinoRR(end) >= 1
-              warning(['celestinoRR >= 1 for: ' siteNames{j} ' ' num2str(evtIdx)]);
+        % Concatenate facts about this event including RR, start and end times, Peak Intensity etc.
+        % Check if there's a "merged" version of the RR. If so, use that instead of the original one.
+        fieldName = ['evt' num2str(evtIdx)];
+        includeBootMerge = false;
+        if isfield(mergeRR.(sites{j}), fieldName) && includeBootMerge
+            thisEvtRR = mean(mergeRR.(sites{j}).(fieldName));
+        else
+            thisEvtRR = allEvents{j}(evtIdx).stats.mod.RR.(cells{i}{2}).precip;
+        end
+        data.(sites{j}).RR = [data.(sites{j}).RR thisEvtRR];
+        % Notify if we just found an event with a RR > 1.
+        if data.(sites{j}).RR(end) >= 1
+          warning(['RR >= 1 for: ' sites{j} ' ' num2str(evtIdx)]);
+        end
+
+        data.(sites{j}).startTimes = [data.(sites{j}).startTimes allEvents{j}(evtIdx).startTime];
+        data.(sites{j}).endTimes = [data.(sites{j}).endTimes allEvents{j}(evtIdx).endTime];
+        data.(sites{j}).PI = [data.(sites{j}).PI allEvents{j}(evtIdx).stats.mod.int.peak.precip];
+        data.(sites{j}).AvgI = [data.(sites{j}).AvgI allEvents{j}(evtIdx).stats.mod.int.avg.precip];
+
+        % Add additional RR data for the MAT site using Celestino.
+        % NOTE: This does not take into account the possible presence of a merged RR for celestino.
+        if strcmpi(sites{j}, 'MAT')
+            data.(sites{j}).celestinoRR = [data.(sites{j}).celestinoRR allEvents{j}(evtIdx).stats.mod.RR.(cells{i}{2}).addl];
+            if data.(sites{j}).celestinoRR(end) >= 1
+              warning(['celestinoRR >= 1 for: ' sites{j} ' ' num2str(evtIdx)]);
             end
-            data.(siteNames{j}).celestinoPI = [data.(siteNames{j}).celestinoPI allEvents{j}(evtIdx).stats.mod.int.peak.celestino];
-            data.(siteNames{j}).celestinoAvgI = [data.(siteNames{j}).celestinoAvgI allEvents{j}(evtIdx).stats.mod.int.avg.celestino];
+            data.(sites{j}).celestinoPI = [data.(sites{j}).celestinoPI allEvents{j}(evtIdx).stats.mod.int.peak.celestino];
+            data.(sites{j}).celestinoAvgI = [data.(sites{j}).celestinoAvgI allEvents{j}(evtIdx).stats.mod.int.avg.celestino];
         end
     end
 
     % Calculate duration
-    data.(siteNames{j}).duration = data.(siteNames{j}).endTimes - data.(siteNames{j}).startTimes;
+    data.(sites{j}).duration = data.(sites{j}).endTimes - data.(sites{j}).startTimes;
     % Change intensities to mm/hr (from mm/10min)
-    data.(siteNames{j}).PI = data.(siteNames{j}).PI * 6;
-    data.(siteNames{j}).AvgI = data.(siteNames{j}).AvgI * 6;
-    data.(siteNames{j}).celestinoAvgI = data.(siteNames{j}).celestinoAvgI * 6;
-    data.(siteNames{j}).celestinoPI = data.(siteNames{j}).celestinoPI * 6;
+    data.(sites{j}).PI = data.(sites{j}).PI * 6;
+    data.(sites{j}).AvgI = data.(sites{j}).AvgI * 6;
+    data.(sites{j}).celestinoAvgI = data.(sites{j}).celestinoAvgI * 6;
+    data.(sites{j}).celestinoPI = data.(sites{j}).celestinoPI * 6;
 
     %% Plot events
     plotScatters = false;
     if plotScatters
         % Plot Runoff Ratio vs Duration
         figure
-        plot(data.(siteNames{j}).RR, data.(siteNames{j}).duration, 'o');
-        title([siteNames{j} ' RR vs Duration for Good Events']);
+        plot(data.(sites{j}).RR, data.(sites{j}).duration, 'o');
+        title([sites{j} ' RR vs Duration for Good Events']);
         xlabel('Runoff Ratio');
         ylabel('Duration');
 
         % Plot Runoff Ratio vs Peak Intensity
         figure
-        plot(data.(siteNames{j}).RR, data.(siteNames{j}).PI, 'o');
-        title([siteNames{j} ' RR vs Peak Intensity for Good Events']);
+        plot(data.(sites{j}).RR, data.(sites{j}).PI, 'o');
+        title([sites{j} ' RR vs Peak Intensity for Good Events']);
         xlabel('Runoff Ratio');
         ylabel('Peak Intensity (mm/hr)');
 
         % Plot Runoff Ratio vs Average Intensity
         figure
-        plot(data.(siteNames{j}).RR, data.(siteNames{j}).AvgI, 'o');
-        title([siteNames{j} ' RR vs Average Intensity for Good Events']);
+        plot(data.(sites{j}).RR, data.(sites{j}).AvgI, 'o');
+        title([sites{j} ' RR vs Average Intensity for Good Events']);
         xlabel('Runoff Ratio');
         ylabel('Average Intensity (mm/hr)');
 
         % Plot extra figures using Celestino data for MAT
-        if strcmpi(siteNames{j}, 'MAT')
+        if strcmpi(sites{j}, 'MAT')
 
             % Plot Runoff Ratio vs Peak Intensity
             figure
-            plot(data.(siteNames{j}).celestinoRR, data.(siteNames{j}).celestinoPI, 'o');
-            title([siteNames{j} ' Celestino RR vs Celestino Peak Intensity for Good Events']);
+            plot(data.(sites{j}).celestinoRR, data.(sites{j}).celestinoPI, 'o');
+            title([sites{j} ' Celestino RR vs Celestino Peak Intensity for Good Events']);
             xlabel('Runoff Ratio');
             ylabel('Peak Intensity (mm/hr)');
 
             % Plot Runoff Ratio vs Average Intensity
             figure
-            plot(data.(siteNames{j}).celestinoRR, data.(siteNames{j}).celestinoAvgI, 'o');
-            title([siteNames{j} ' Celestino RR vs Celestino Average Intensity for Good Events']);
+            plot(data.(sites{j}).celestinoRR, data.(sites{j}).celestinoAvgI, 'o');
+            title([sites{j} ' Celestino RR vs Celestino Average Intensity for Good Events']);
             xlabel('Runoff Ratio');
             ylabel('Average Intensity (mm/hr)');
         end
@@ -118,8 +136,7 @@ display(['PAS RR is: ' num2str((pasRR - matRR) / matRR * 100) '% higher than MAT
 
 % Now that we've extracted data for both MAT and PAS, generate plots with both.
 % First, sort the RR data into bins for the box plots.
-% Need all bin edges to be uniform between MAT and PAS, so use the one with the
-% widest range to set the bin edges for the other.
+% Need all bin edges to be uniform between MAT and PAS.
 numBins = 5;
 edges = 0:0.2:1;
 [~, ~, binsMAT]  = histcounts(data.MAT.RR, edges);
@@ -159,11 +176,17 @@ MATPASGap = 1;
 lineSize = 3;
 textSize = 18;
 axisFontSize = textSize;
+if includeBootMerge
+    titleText = '"Good" and bootstrapped-merged data combined.';
+else
+    titleText = '"Good" data only, no bootstrapped-merged data.';
+end
 
 % Plot peak intensity boxes.
 figure
-bh = boxplot(peakIntensity, {bins, IDs}, 'Colors', 'rb',  'Labels', boxLabels,...
+bh = boxplot(peakIntensity, {bins, IDs},  ...
         'FactorGap', [binGap, MATPASGap], 'Symbol', '+');
+        % 'Colors', 'rb', 'Labels', boxLabels,
 set(bh(:), 'linewidth', lineSize);
 set(gca,'FontSize',axisFontSize)
 % bh(:,2).linewidth = 6;
@@ -171,30 +194,34 @@ set(gca,'FontSize',axisFontSize)
 ylabel('Peak Intensity (mm/hr)', 'FontSize', textSize);
 xlabel('Runoff Ratio', 'FontSize', textSize);
 % Turn on the legend (different colors for MAT and PAS).
-legend(findobj(gca, 'Tag', 'Box'), {'PAS', 'MAT'}, 'FontSize', textSize);
-
+% legend(findobj(gca, 'Tag', 'Box'), {'PAS', 'MAT'}, 'FontSize', textSize);
+title(titleText);
 
 % Plot intensity boxes.
 figure
-bh = boxplot(intensity, {bins, IDs}, 'Colors', 'rb',  'Labels', boxLabels,...
+bh = boxplot(intensity, {bins, IDs}, ...
         'FactorGap', [binGap, MATPASGap], 'Symbol', '+');
+        % 'Colors', 'rb',  'Labels', boxLabels,
 set(bh(:), 'linewidth', lineSize);
 set(gca,'FontSize',axisFontSize)
 % title('RR vs Mean Intensity for Good Events');
 ylabel('Mean Intensity (mm/hr)', 'FontSize', textSize);
 xlabel('Runoff Ratio', 'FontSize', textSize);
 % Turn on the legend (different colors for MAT and PAS).
-legend(findobj(gca, 'Tag', 'Box'), {'PAS', 'MAT'}, 'FontSize', textSize);
+% legend(findobj(gca, 'Tag', 'Box'), {'PAS', 'MAT'}, 'FontSize', textSize);
+title(titleText);
 
 
 % Plot duration boxes.
 figure
-bh = boxplot(duration, {bins, IDs}, 'Colors', 'rb',  'Labels', boxLabels,...
+bh = boxplot(duration, {bins, IDs}, ...
         'FactorGap', [binGap, MATPASGap], 'Symbol', '+');
+        % 'Colors', 'rb',  'Labels', boxLabels,
 set(bh(:), 'linewidth', lineSize);
 set(gca,'FontSize',axisFontSize)
 % title('RR vs Duration for Good Events');
 ylabel('Event Duration (minutes)', 'FontSize', textSize);
 xlabel('Runoff Ratio', 'FontSize', textSize);
 % Turn on the legend (different colors for MAT and PAS).
-legend(findobj(gca, 'Tag', 'Box'), {'PAS', 'MAT'}, 'FontSize', textSize);
+% legend(findobj(gca, 'Tag', 'Box'), {'PAS', 'MAT'}, 'FontSize', textSize);
+title(titleText);
